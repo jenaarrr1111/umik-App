5c993799659eb8ba23772f1399c96846f2fe8b01
jenaarrr1111 <nazaralif3009@gmail.com>
2023-02-01 00:58:39 +0700

    Membuat folder penjual & seller_welcome

---
+++ lib/screens/penjual/seller_welcome/components/body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:umik/constants.dart';
+import 'package:umik/screens/sign_in/sign_in_screen.dart';
+import 'package:umik/size_config.dart';
+
+// This is the best practice
+import '../components/seller_welcome_content.dart';
+import '../../../../components/default_button.dart';
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
