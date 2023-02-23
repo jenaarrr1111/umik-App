89879bffbeba28616b96c85f0942c78533b8be5f
markus-langgeng <markus.langgeng4@gmail.com>
2023-02-23 09:35:05 +0700

    Memperbaiki layout seller_promo_listview

--- lib/screens/penjual/promo/components/seller_promo_listview.dart
+++ lib/screens/penjual/promo/components/seller_promo_listview.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:umik/constants.dart';
-import 'package:umik/screens/penjual/edit_produk/seller_edit_product.dart';
+// import 'package:umik/screens/penjual/edit_produk/seller_edit_product.dart';
 
-// ignore: must_be_immutable
-class SellerPromoGridView extends StatefulWidget {
+class SellerPromoListView extends StatefulWidget {
   final String namaMenu, thumbnail, harga, status, keterangan;
 
-  const SellerPromoGridView({
+  const SellerPromoListView({
     Key? key,
     required this.namaMenu,
     required this.thumbnail,
@@ -16,102 +15,103 @@ class SellerPromoGridView extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<SellerPromoGridView> createState() => _SellerMenuGridViewState();
+  State<SellerPromoListView> createState() => _SellerMenuListViewState();
 }
 
-class _SellerMenuGridViewState extends State<SellerPromoGridView> {
-  bool isOn = false;
-
+class _SellerMenuListViewState extends State<SellerPromoListView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromARGB(217, 217, 217, 217),
+      color: const Color.fromARGB(217, 217, 217, 217),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: SizedBox(
-                  width: 110.0,
-                  height: 100.0,
-                  child: Image.asset(
-                    widget.thumbnail,
-                    fit: BoxFit.contain,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: SizedBox(
+                    width: 110.0,
+                    height: 100.0,
+                    child: Image.asset(
+                      widget.thumbnail,
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 15, // <-- SEE HERE
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.namaMenu,
-                      textAlign: TextAlign.left,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    SizedBox(
-                      height: 9, // <-- SEE HERE
-                    ),
-                    Text(widget.harga,
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.titleSmall),
-                    SizedBox(
-                      height: 9, // <-- SEE HERE
-                    ),
-                    Text(widget.status,
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.namaMenu,
                         textAlign: TextAlign.left,
                         style: Theme.of(context)
                             .textTheme
                             .titleLarge!
-                            .copyWith(color: Color.fromARGB(255, 19, 110, 22))),
-                  ],
-                ),
-              ),
-              Builder(builder: (context) {
-                if (widget.status == 'Selesai') {
-                  return Container(
-                    margin: EdgeInsets.only(top: 55),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: kPrimaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
+                            .copyWith(fontSize: 15),
                       ),
-                      child: Text(
-                        'Ulangi',
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium!
-                            .copyWith(fontSize: 12),
+                      const SizedBox(height: 9),
+                      Text(widget.harga,
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context).textTheme.titleSmall),
+                      const SizedBox(height: 9),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              widget.status,
+                              textAlign: TextAlign.left,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(
+                                    color:
+                                        const Color.fromARGB(255, 19, 110, 22),
+                                    fontSize: 15,
+                                  ),
+                            ),
+                          ),
+                          (widget.status == 'Selesai')
+                              ? ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 15),
+                                    backgroundColor: kPrimaryColor,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Ulangi',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelMedium!
+                                        .copyWith(fontSize: 12),
+                                  ),
+                                )
+                              : const SizedBox(),
+                        ],
                       ),
-                    ),
-                  );
-                } else {
-                  return Text(' ');
-                }
-              }),
-              Align(
-                child: IconButton(
-                  iconSize: 15.0,
-                  icon: isOn
-                      ? const Icon(Icons.toggle_on_rounded,
-                          color: kPrimaryColor)
-                      : const Icon(Icons.arrow_forward_ios_rounded),
-                  onPressed: () {
-                    setState(() {
-                      isOn = !isOn;
-                    });
-                  },
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Align(
+                  child: IconButton(
+                    iconSize: 15.0,
+                    icon: const Icon(Icons.arrow_forward_ios_rounded),
+                    onPressed: () {
+                      // TODO: Nanti diubah ke edit promo (bukan add promo)
+                      Navigator.pushNamed(context, '/add_promo');
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
